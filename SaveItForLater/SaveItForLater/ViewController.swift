a6d9b3fc841665c028357e3ff500944a947f0289
Jamie Lockhart <james@hotspotparking.ca>
2017-01-19 14:38:29 -0500

    boiler plate

---
+++ SaveItForLater/SaveItForLater/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveItForLater
+//
+//  Created by Jamie Lockhart on 2017-01-19.
+//  Copyright © 2017 iLockhart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
